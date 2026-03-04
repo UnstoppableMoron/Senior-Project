66da63fff6a1bd0f32ed3457a6a4e8130efd5321
UnstoppableMoron <cte-owen.baker@sllboces.org>
2026-03-04 15:18:57 +0000

    It's so over

--- ballin.py
+++ ballin.py
@@ -1,5 +1,23 @@
 # hi
-# Dude, I saw Kainan at Stewart's Saturday, it was awesome
 # How do we feel about gambling
 
-print()
+
+class Player:
+    """This class details the basic characteristics of the player character"""
+
+class Enemy:
+    """This class details the basic characteristics of common enemies"""
+
+class Location:
+    """This class details the basic characteristics of various locations"""
+
+class Item:
+    """This class details the basic characteristics of certain items"""
+
+def start_game():
+    print("Every hero needs a name! What's yours?")
+    playerName = str(input("Name your likeness: "))
+    print(f"Well, {playerName}, I hope your adventure is as enjoyable for you as it's sure to be for me.")
+    print("May the dice serve you well.")
+
+start_game()
